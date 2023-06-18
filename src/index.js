69669ce7911cf7858db229a0cbddc907a139e3b7
Tanish <tanishvora99@gmail.com>
2023-06-18 17:41:49 +0530

    index js changed

--- src/index.js
+++ src/index.js
@@ -1,30 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from 'react-router-dom';
-import { UserProvider } from './contexts/user.context';
-import CategoriesProvider from './contexts/categories.context';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
+import App from './App';
+import { UserProvider } from './contexts/user.context';
+import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import './index.scss';
+
+const rootElement = document.getElementById('root');
+
+render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
         <CategoriesProvider>
           <CartProvider>
-              <App />
+            <App />
           </CartProvider>
         </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
+  rootElement
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
